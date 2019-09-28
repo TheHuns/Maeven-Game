9750269ab0ba3e359ad032e6d29ed0f494f11dbc
Dehn Hunsworth <dhunsworth@yahoo.com>
2019-09-28 13:14:55 -0600

    basic layout

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Card from "./components/Card";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Test</Text>
+      <Text style={styles.title}>Funny Bunny</Text>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </View>
   );
 }
@@ -13,7 +20,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#2d2d2d",
-    alignItems: "center",
-    justifyContent: "center"
+    flexDirection: "row",
+    flexWrap: "wrap",
+    padding: 25,
+    paddingVertical: "12%",
+    justifyContent: "space-around"
+  },
+  title: {
+    color: "lightblue",
+    width: "100%",
+    fontSize: 22,
+    textAlign: "center",
+    paddingBottom: 10
   }
 });
--- components/Card.js
+++ components/Card.js
@@ -1,18 +1,31 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
-export default function Card() {
-  return (
-    <View style={styles.cardContainer}>
-      <Text>Card</Text>
-    </View>
-  );
+export default class Card extends React.Component {
+  state={
+    cardFacing: 
+  }
+  
+  changeBackground() => {
+    this.set
+  }
+  render() {
+    return (
+      <TouchableOpacity style={styles.cardContainer} onPress={() => }>
+        <Text>Card</Text>
+      </TouchableOpacity>
+    );
+
+  }
+
+
 }
 
 const styles = StyleSheet.create({
   cardContainer: {
-    background: "lightblue",
+    backgroundColor: "lightblue",
     height: "25%",
-    width: "40%"
+    width: "40%",
+    margin: 5
   }
 });
